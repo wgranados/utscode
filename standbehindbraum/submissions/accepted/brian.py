425164127d666c7db58f06d78ff37e9ebe17002c
Brian <catzwillruletheearth@gmail.com>
2017-03-15 22:31:23 -0400

    updated Q1 solution in python

---
+++ standbehindbraum/submissions/accepted/brian.py
@@ -0,0 +1,43 @@
+import math
+
+# Stand Behind Braum Solution
+# UTSCode 2017
+# Not for public eyes
+# @Author: Brian Chen
+
+def damageCalc(inc, res):
+    # Reduction fraction
+    reduc = (100 / (100 + res))
+    dmg = inc * reduc
+    return dmg
+
+if __name__ == '__main__':
+    # Parse input lines
+    firstLine = input()
+    fl_items = firstLine.split()
+    # Percentages
+    phys = float(fl_items[0])
+    mgc = float(fl_items[1])
+    tru = float(fl_items[2])
+    # Total Damage
+    tot = float(fl_items[3])
+    
+    # Parse input lines
+    secondLine = input()
+    sl_items = secondLine.split()
+    # Resistances
+    arm = float(sl_items[0])
+    mr = float(sl_items[1])
+    
+    # Incoming damage calculations
+    physDmg = phys * tot
+    mgcDmg = mgc * tot
+    truDmg = tru * tot
+    
+    # Totalling all the taken damage
+    dmgTaken = truDmg
+    dmgTaken += damageCalc(physDmg, arm)
+    dmgTaken += damageCalc(mgcDmg, mr)
+    dmgTaken = math.floor(dmgTaken)
+    
+    print(dmgTaken)
