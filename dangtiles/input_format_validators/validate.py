ca479bfa670da01fcbda5021d29fc0aa180c5d8f
William Granados <me@wgma00.me>
2017-03-16 17:26:27 -0400

    fixed validation and output formatlt validator for dangtile

--- dangtiles/input_format_validators/validate.py
+++ dangtiles/input_format_validators/validate.py
@@ -3,22 +3,25 @@ from sys import stdin
 import re
 import sys
 
-integer = "(0|-?[1-9]\d*)"
-four_ints = re.compile('{a} {b} {c} {d}\n'.format(a=integer, b=integer, c=integer, d=integer))
-three_ints = re.compile('{a} {b} {c}\n'.format(a=integer, b=integer, c=integer))
-two_ints = re.compile('{a} {b}\n'.format(a=integer, b=integer, c=integer))
-single_integer = re.compile(integer)
 
 line = stdin.readline()
-assert four_ints.match(line)
-n, m, e, u = map(int, line.strip().split())
+try:
+    n, m, e, u = map(int, line.strip().split())
+except ValueError as e:
+    assert False
 assert 1 <= n <= 20 and 1 <= m <= 20
 
 for i in range(e):
     line = stdin.readline()
-    assert three_ints.match(line)
+    try:
+        a, b, c = map(int, line.strip().split())
+    except ValueError as e:
+        assert False
 
 for i in range(u):
     line = stdin.readline()
-    assert two_ints.match(line)
+    try:
+        a, b = map(int, line.strip().split())
+    except ValueError as e:
+        assert False
 sys.exit(42)
