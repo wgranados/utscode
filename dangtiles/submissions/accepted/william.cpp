4a44e98894f75f280e85509e413e20202f3c8e97
William Granados <me@wgma00.me>
2017-03-16 04:44:12 -0400

    added solution and also fixed sample input for dangtiles

--- dangtiles/submissions/accepted/william.cpp
+++ dangtiles/submissions/accepted/william.cpp
@@ -1,48 +1,98 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N, M;
-int A[5001][5001];
-
+int N, M, E, U;
 int dr[] = {-1, 0, 1, 0};
 int dc[] = {0, 1, 0, -1};
+bool v[50][50];
+vector<pair<int,pair<int,int> > >enemies;
+vector<pair<int,int> >units;
 
-bool within_range(int r, int c) {
-    return 0 <= r && r < N && 0 <= c && c < M;
+bool valid(int r, int c) {
+    return r >= 0 && r < N && c >= 0 && c < M;
 }
 
-bool is_possible(int r, int c) {
-    for(int d = 0;d < 4;d++) {
-        if(within_range(r+dr[d], c+dc[d]) && A[r+dr[d]][c+dc[d]] != 0){
-            return true;
+void dfs(int r, int c, int m) {
+    if(m >= 0){
+        v[r][c] = true;
+        for(int d = 0;d < 4;d++) {
+            int nr = r+dr[d];
+            int nc = c+dc[d];
+            if(valid(nr, nc)) {
+                dfs(nr, nc, m-1);
+            }
         }
     }
-    return false;
+}
+
+vector< pair<int,int> >new_loc;
+void dfs_brute(int troop){
+    if(troop >= 0){
+        // try branching out in 4 standard directions
+        for(int d = 0;d < 4;d++) {
+            int nr = units[troop].first + dr[d];
+            int nc = units[troop].second + dc[d];
+            if(valid(nr, nc) && !v[nr][nc]) {
+                new_loc.push_back(make_pair(nr, nc));
+                dfs_brute(troop-1);
+                new_loc.pop_back();
+            }
+        }
+        // try staying in the same place
+        int nr = units[troop].first;
+        int nc = units[troop].second;
+        if(!v[nr][nc]) {
+            new_loc.push_back(make_pair(nr, nc));
+            dfs_brute(troop-1);
+            new_loc.pop_back();
+        }
+    }else {
+        // checking for validaty of generated
+        // movement of units
+        set< pair<int,int> >check_for_int;
+        for(int i = 0;i < new_loc.size();i++) {
+            if(!check_for_int.count(new_loc[i])) {
+                check_for_int.insert(new_loc[i]);
+            } else{
+                check_for_int.clear();
+                return;
+            }
+        }
+        for(int i = new_loc.size()-1;i >= 0;i--){
+            cout << new_loc[i].first+1 << " " << new_loc[i].second+1 << endl;
+        }
+        exit(0);
+    }
+}
+
+void debug(int n, int m) {
+    for(int i = 0;i < n;i++) {
+        for(int j = 0;j < m;j++) {
+            cout << (v[i][j] ? "1":"0");
+        }
+        cout << endl;
+    }
 }
 
 int main() {
-    cin >> N >> M;
-    for(int i = 0;i < N;i++) {
-        for(int j = 0;j < M;j++) {
-            cin >> A[i][j];
-        }
+    cin >> N >> M >> E >> U;
+    memset(v, false, sizeof(v));
+    for(int i = 0;i < E;i++) {
+        int x,y,m;
+        cin >> x >> y >> m;
+        enemies.push_back(make_pair(x-1, make_pair(y-1, m)));
     }
-    int r = 0, c = 0, ans = 0;
-    while(is_possible(r, c)) {
-        int best = 0, dir = 0;
-        for(int d = 0;d < 4;d++){
-            if(within_range(r+dr[d], c+dc[d]) && A[r+dr[d]][c+dc[d]] != 0){
-                if(best < A[r+dr[d]][c+dc[d]] ){
-                    best = A[r+dr[d]][c+dc[d]];
-                    dir = d;
-                }
-            }
-        }
-        ans += best;
-        r += dr[dir];
-        c += dc[dir];
-        A[r][c] = 0;
+    for(int i = 0;i < U;i++){
+        int x,y;
+        cin >> x >> y;
+        units.push_back(make_pair(x-1,y-1));
     }
-    cout << ans << endl;
+    for(int i = 0;i < E;i++){
+        // flood fill one more space to check to mark movement and attack range
+        dfs(enemies[i].first, enemies[i].second.first, enemies[i].second.second+1);
+    }
+    dfs_brute(units.size()-1);
+    // dfs_brute exectures exit(0) on successful brute force
+    cout << "soft-reset!" << endl;
     return 0;
 }
