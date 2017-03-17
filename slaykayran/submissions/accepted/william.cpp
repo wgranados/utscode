210d2f009e07feaa78765e63cb49e1848daed65c
William Granados <me@wgma00.me>
2017-03-17 03:06:16 -0400

    updated testdat data anf validation for slaykayran

--- slaykayran/submissions/accepted/william.cpp
+++ slaykayran/submissions/accepted/william.cpp
@@ -8,10 +8,29 @@ int N,H,R;
 int rules[MAXN];
 int DP[MAXN][MAXH];
 
-void rec(int num_heads, int cur_health) {
-    int next_head_if_attack = rules[num_heads];
-    int next_health_if_attack = cur_health-num_heads;
-
+int rec(int num_heads, int cur_health) {
+    //cout << num_heads <<  " " << cur_health << endl;
+    if(num_heads == 0) {
+        return 0;
+    }
+    else if (DP[num_heads][cur_health] != INF){
+        return DP[num_heads][cur_health];
+    }
+    else {
+        // try defending
+        int num_head_if_defend = rules[num_heads];
+        int cur_health_if_defend  = cur_health-1;
+        if(cur_health_if_defend > 0) {
+            DP[num_heads][cur_health] = min(DP[num_heads][cur_health], 1+rec(num_head_if_defend, cur_health_if_defend));
+        }
+        // try attacking
+        int num_head_if_attack = rules[num_heads-1];
+        int cur_health_if_attack = cur_health-num_heads;
+        if(cur_health_if_attack > 0) {
+            DP[num_heads][cur_health] = min(DP[num_heads][cur_health], 1+rec(num_head_if_attack, cur_health_if_attack));
+        }
+        return DP[num_heads][cur_health];
+    }
 }
 
 int main() {
@@ -23,8 +42,12 @@ int main() {
     for(int i = 0;i < R;i++) {
         int n_i, n_j;
         cin >> n_i >> n_j;
-        rules[n_j] = n_j;
+        rules[n_i] = n_j;
     }
-    rec(N, H);
+    int ans = rec(N, H);
+    if(ans == INF)
+        cout << "retreat" << endl;
+    else
+        cout << ans << endl;
     return 0;
 }
