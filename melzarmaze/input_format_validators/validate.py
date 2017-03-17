fd58f326ae8fd6962e6905d2c210d71ff1a0d492
William Granados <me@wgma00.me>
2017-03-17 04:15:29 -0400

    fized so it gets no warnings

--- melzarmaze/input_format_validators/validate.py
+++ melzarmaze/input_format_validators/validate.py
@@ -8,14 +8,19 @@ two_int_first_line = re.compile(integer + " " + integer + "\n")
 single_integer = re.compile(integer)
 
 line = stdin.readline()
-assert two_int_first_line.match(line)
-n, m = map(int, line.split())
-assert 0 <= n <= 5000 and 0 <= m <= 5000
+try:
+    n, m = map(int, line.split())
+except ValueError as e:
+    assert False
+
+assert 1 <= n, m <= 5000
 
 for i in range(n):
-    elements = stdin.readline().strip().split(' ')
+    elements = stdin.readline().strip().split()
     assert len(elements) == m
     for j in range(m):
-        assert single_integer.match(elements[j])
-        assert 0 <= int(elements[j]) <= 2**31-1
+        try:
+            n = int(elements[j])
+        except ValueError:
+            assert False
 sys.exit(42)
