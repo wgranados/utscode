b83f6239224239b1e8f505a6c85d536cc7b9ef9c
William Granados <me@wgma00.me>
2017-03-22 19:19:40 -0400

    testing to find out the smallest texlive modules nessary for our build

--- travis-install.sh
+++ travis-install.sh
@@ -11,8 +11,10 @@ apt-get install libgmp3-dev
 apt-get install automake
 apt-get install autoconf
 apt-get install texlive
-apt-get install texlive-recomendded
-apt-get install texlive-cryllic
+apt-get install texlive-latex-recommended
+apt-get install texlive-fonts-recommended
+apt-get install texlive-latex-extra
+apt-get install texlive-lang-cyrillic
 apt-get install texlive-lang-english
 apt-get install tidy
 apt-get install ghostscript
