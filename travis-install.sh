b3f34d9c89024a8772961aaa74e6d10ce9501bd9
William Granados <william.granadosmatamoros@mail.utoronto.ca>
2017-03-22 20:33:18 -0400

    Update travis-install.sh
    Made build smaller by minimising the amount of latex dependencies needed to compile the problem set.

--- travis-install.sh
+++ travis-install.sh
@@ -1,16 +1,15 @@
-#!/usr/bin/env bash
 apt-get install python-pkg-resources
 apt-get install libboost-regex-dev
-apt-get install libc6
-apt-get install libgcc1
-apt-get install libgmp10
-apt-get install libgmpxx4ldbl
-apt-get install libstdc++6
 apt-get install debhelper
 apt-get install libgmp3-dev
 apt-get install automake
 apt-get install autoconf
-apt-get install texlive-full
+apt-get install texlive
+apt-get install texlive-latex-recommended
+apt-get install texlive-fonts-recommended
+apt-get install texlive-latex-extra
+apt-get install texlive-lang-cyrillic
+apt-get install texlive-lang-english
 apt-get install tidy
 apt-get install ghostscript
 pip2 install pytest
